9ecd1923d575fe0df28e5aedd3ec4fce0fa6426b
Dmitriy Metelya <dmitriymetelya@mail.ru>
2021-02-02 18:56:10 +0700

    calc.js fixed

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,23 +2,23 @@ import random from '../libs/random.js';
 import playBrainGame from '../index.js';
 
 const question = 'What is the result of the expression?';
-
-const calculate = (firstNum, secondNum, operationType) => {
-  const operations = {
-    '+': (a, b) => a + b,
-    '-': (a, b) => a - b,
-    '*': (a, b) => a * b,
-  };
-
-  return operations[operationType](firstNum, secondNum);
+const operations = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
 };
+const calculate = (
+  firstNum,
+  secondNum,
+  operationType,
+) => operations[operationType](firstNum, secondNum);
 
 const generateRound = () => {
   const round = {};
   const firstNum = random();
   const secondNum = random();
   const operationTypes = ['+', '-', '*'];
-  const operationIndex = random(0, 2);
+  const operationIndex = random(0, operationTypes.length);
 
   round.question = `${firstNum} ${operationTypes[operationIndex]} ${secondNum}`;
   round.correctAnswer = calculate(firstNum, secondNum, operationTypes[operationIndex]).toString();
